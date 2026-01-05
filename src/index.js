5664f0b33449d4e19c561ed8ba39bd9f12808752
Lydia Khasoa <khasoalydia@gmail.com>
2026-01-06 01:11:56 +0300

    complete tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+//Connect the html form
+const form = document.getElementById("create-task-form");
+
+//Add an event listener to the form
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+//Get the task decription from the form
+    const task = event.target.querySelector("#new-task-description").value;
+    buildToDo(task);
+
+});
+
+//Add item to list and display
+function buildToDo(task) {
+    const li = document.createElement("li");
+    li.textContent = task;
+
+    const tasklist = document.getElementById("tasks");
+    tasklist.appendChild(li);
+}
 });
